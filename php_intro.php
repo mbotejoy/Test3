e2227d15162882b9a683fddbfa53e04876ebd16c
mbotejoy <joy.mbote@strathmore.edu>
2024-05-20 16:27:39 +0300

    Intro to php

---
+++ php_intro.php
@@ -0,0 +1,15 @@
+<?php
+//Comments written in double slashes
+print "My first php statement";
+print '<br>'; //use html to break
+echo"Another way to print the statements,use 'echo'";
+print "<br>";
+print "Termination is done using a semi colon";
+print'<br>';
+print 21+1;//Displays the result
+print'<br>';
+print 19;//Displayes the number
+
+
+
+?>
